8d424c870d2b8f27d9fe593b45dd32e144449af4
Julian Fleischer <julian.fleischer@gamegenetics.de>
2015-12-14 10:17:22 +0100

    better code coverage

--- test/index.js
+++ test/index.js
@@ -45,6 +45,92 @@ describe('white-horse-logger', function () {
     });
   });
 
+  it('should log on six default levels', function (done) {
+    var container = new WhiteHorse(require);
+    container.use('../index.js');
+    var messages = [];
+    var transport = function (message) {
+      messages.push(message);
+    };
+    transport.$factory = false;
+    container.register('$loggerTransport', transport);
+    container.inject(function ($logger) {
+      $logger.trace("one %03d", 1);
+      $logger.debug("two %03d", 2);
+      $logger.info("three %03d", 3);
+      $logger.warn("four %03d", 4);
+      $logger.error("five %03d", 5);
+      $logger.fatal("six %03d", 6);
+    }, function () {
+      assert.equal(messages.length, 6, 'there should be six messages');
+      assert(/^TRACE +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +one 001$/.test(messages[0]));
+      assert(/^DEBUG +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +two 002$/.test(messages[1]));
+      assert(/^INFO +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +three 003$/.test(messages[2]));
+      assert(/^WARN +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +four 004$/.test(messages[3]));
+      assert(/^ERROR +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +five 005$/.test(messages[4]));
+      assert(/^FATAL +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +six 006$/.test(messages[5]));
+      done();
+    });
+  });
+
+  it('should log custom levels', function (done) {
+    var container = new WhiteHorse(require);
+    container.use('../index.js');
+    var messages = [];
+    var transport = function (message) {
+      messages.push(message);
+    };
+    transport.$factory = false;
+    container.register('$loggerTransport', transport);
+    container.register('$loggerLogLevels', {
+      nevermind: 2,
+      problem: 4,
+      issue: 6
+    });
+    container.inject(function ($logger) {
+      $logger.nevermind("one %03d", 1);
+      $logger.problem("two %03d", 2);
+      $logger.issue("three %03d", 3);
+    }, function () {
+      assert.equal(messages.length, 3, 'there should be three messages');
+      assert(/^NEVERMIND +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +one 001$/.test(messages[0]));
+      assert(/^PROBLEM +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +two 002$/.test(messages[1]));
+      assert(/^ISSUE +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +three 003$/.test(messages[2]));
+      done();
+    });
+  });
+  
+  it('should log custom levels with a configured $root logger', function (done) {
+    var container = new WhiteHorse(require);
+    container.use('../index.js');
+    var messages = [];
+    var transport = function (message) {
+      messages.push(message);
+    };
+    transport.$factory = false;
+    container.register('$loggerTransport', transport);
+    container.register('$loggerLogLevels', {
+      nevermind: 2,
+      problem: 4,
+      issue: 6
+    });
+    container.register('$loggerConfig', {
+      $root: {
+        level: 'problem'
+      }
+    });
+    container.inject(function ($logger) {
+      $logger.nevermind("one %03d", 1);
+      $logger.problem("two %03d", 2);
+      $logger.issue("three %03d", 3);
+    }, function () {
+      assert.equal(messages.length, 2, 'there should be two messages');
+      assert(/^PROBLEM +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +two 002$/.test(messages[0]));
+      assert(/^ISSUE +[0-9\-]+T[0-9:\.]+Z +\[\$root\] +three 003$/.test(messages[1]));
+      done();
+    });
+  });
+  
   it('should inject the right logger into the right module', function (done) {
     var container = new WhiteHorse(require);
     container.use('../index.js');
